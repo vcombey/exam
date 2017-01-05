a3c76a1119790adcc3b56549723ba410e264dca8
Vincent Combey <Vcombey@Vincents-MacBook-Pro.local>
2017-01-05 17:37:48 +0100

    premier commit

--- next2.c
+++ next2.c
@@ -75,20 +75,13 @@ void	ft_next(char *str)
 		printf("Error\n");
 		return ;
 	}
-
-	printf("cest passe\n");
 	i = 0;
 	a = atoi(str);
 	while ((str[i] >= '0') && (str[i] <= '9'))
 		i++;
 	b = atoi(str + i + 1);
 	i++;
-	while ((str[i] >= '0') && (str[i] <= '9'))
-		i++;
-	c = atoi(str + i + 1);
-	i++;
-	printf("a = %d, b = %d, c = %d\n", a, b, c);
-	if ((a != 0) && (c >= b) && (b >= a))
+	if ((a != 0) && (b >= a))
 	{
 		q = b / a;
 		while (b <= c && ft_check_mult(c, q, b))
